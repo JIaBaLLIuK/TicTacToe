4a2eb267cfe02762307b1be3ffb9f8c49cf8735f
JIaBaLLIuK <egortkacev251@gmail.com>
2022-06-30 20:53:51 +0300

    Added the ability to play with a person

--- TicTacToe/TicTacToe/main.c
+++ TicTacToe/TicTacToe/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <Windows.h>
 #include <time.h>
+#include <stdbool.h>
 
 char gameBoard[3][3];
 
@@ -18,55 +19,117 @@ int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	int freeCells;
-	char winner = ' ';
-	int stopPlayingWithBot = 0;
+	bool isFinishedGame = false;
 	do
 	{
 		system("CLS");
-		ResetBoard();
-		do
+		int freeCells;
+		char winner = ' ';
+		printf("Выберите действие:\n1. Игра с компьютером.\n2. Игра с другим человеком.\n0. Завершить работу.\n");
+		int gameType = InputAndCheck(0, 2);
+		switch (gameType)
 		{
-			system("CLS");
-			printf("Ход компьютера...\n");
-			Sleep(250);
-			ComputerMove();
-			PrintBoard();
-			winner = CheckWinner();
-			if (winner == 'O' || winner == 'X')
+		case 1:
+		{
+			int stopPlayingWithBot = 0;
+			do
 			{
-				PrintWinner(winner);
-				break;
-			}
-			freeCells = CheckFreeSpace();
-			if (freeCells == 0)
+				system("CLS");
+				ResetBoard();
+				do
+				{
+					system("CLS");
+					printf("Ход компьютера...\n");
+					Sleep(250);
+					ComputerMove();
+					PrintBoard();
+					winner = CheckWinner();
+					if (winner == 'O' || winner == 'X')
+					{
+						PrintWinner(winner);
+						break;
+					}
+					freeCells = CheckFreeSpace();
+					if (freeCells == 0)
+					{
+						winner = CheckWinner();
+						PrintWinner(winner);
+						break;
+					}
+					printf("Ваш ход!\n");
+					PlayerMove('X');
+					PrintBoard();
+					winner = CheckWinner();
+					if (winner == 'O' || winner == 'X')
+					{
+						PrintWinner(winner);
+						break;
+					}
+					freeCells = CheckFreeSpace();
+					if (freeCells == 0)
+					{
+						winner = CheckWinner();
+						PrintWinner(winner);
+						break;
+					}
+				} while (freeCells > 0);
+				printf("Повторить игру с компьютером?\n0 - да\n1 - нет\n");
+				stopPlayingWithBot = InputAndCheck(0, 1);
+			} while (!stopPlayingWithBot);
+			break;
+		}
+		case 2:
+		{
+			int stopPlayingWithPerson = 0;
+			do
 			{
-				winner = CheckWinner();
-				PrintWinner(winner);
-				break;
-			}
-			printf("Ваш ход!\n");
-			PlayerMove('X');
-			PrintBoard();
-			winner = CheckWinner();
-			if (winner == 'O' || winner == 'X')
-			{
-				PrintWinner(winner);
-				break;
-			}
-			freeCells = CheckFreeSpace();
-			if (freeCells == 0)
-			{
-				winner = CheckWinner();
-				PrintWinner(winner);
-				break;
-			}
-		} while (freeCells > 0);
-		printf("Повторить игру?\n0 - да\n1 - нет\n");
-		stopPlayingWithBot = InputAndCheck(0, 1);
-	} while (!stopPlayingWithBot);
-	rewind(stdin);
-	char pause = getchar();
+				system("CLS");
+				ResetBoard();
+				do
+				{
+					printf("Ход игрока \"O\"!\n");
+					PlayerMove('O');
+					PrintBoard();
+					winner = CheckWinner();
+					if (winner == 'O' || winner == 'X')
+					{
+						PrintWinner(winner);
+						break;
+					}
+					freeCells = CheckFreeSpace();
+					if (freeCells == 0)
+					{
+						winner = CheckWinner();
+						PrintWinner(winner);
+						break;
+					}
+					printf("Ход игрока \"X\"!\n");
+					PlayerMove('X');
+					PrintBoard();
+					winner = CheckWinner();
+					if (winner == 'O' || winner == 'X')
+					{
+						PrintWinner(winner);
+						break;
+					}
+					freeCells = CheckFreeSpace();
+					if (freeCells == 0)
+					{
+						winner = CheckWinner();
+						PrintWinner(winner);
+						break;
+					}
+				} while (freeCells > 0);
+				printf("Повторить игру с человеком?\n0 - да\n1 - нет\n");
+				stopPlayingWithPerson = InputAndCheck(0, 1);
+			} while (!stopPlayingWithPerson);
+			break;
+		}
+		case 0:
+			isFinishedGame = true;
+			break;
+		}
+	} while (!isFinishedGame);
 	return 0;
 }
 
